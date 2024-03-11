1c7b8ca08907342ddc8b22dbe59e9bf283bfb50d
Sally Chen <23687599@student.uwa.edu.au>
2024-03-11 21:45:59 +0800

    History 1.0

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* Swiper */
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
@@ -8,4 +9,27 @@ const swiper = new Swiper(".swiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+
+/* Email Me */
+const contact = document.querySelector("#contact");
+contact.addEventListener("click", function () {
+  window.location.href = "mailto:23687599@student.uwa.edu.au";
+});
+
+/* Card Flip */
+const cards = document.querySelectorAll(".card");
+
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    card.classList.toggle("flipped");
+  });
+});
+
+/* Open in a new window */
+const newWindow = document.querySelectorAll(".new-window");
+newWindow.forEach((window) => {
+  window.addEventListener("click", () => {
+    window.open(window.href);
+  });
 });
