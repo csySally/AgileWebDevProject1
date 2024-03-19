fc57baea3bd52061c5ab717519e8edf0b9f0baff
Sally Chen <23687599@student.uwa.edu.au>
2024-03-19 18:29:44 +0800

    Set the architechture of the project

---
+++ js/example.js
@@ -0,0 +1,5 @@
+/* Email Me */
+const contact = document.querySelector("#contact");
+contact.addEventListener("click", function () {
+  window.location.href = "mailto:23687599@student.uwa.edu.au";
+});
