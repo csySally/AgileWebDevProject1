2e8255d298703f3a66d8f1cfe4c5ac61c2307d1e
Sally Chen <23687599@student.uwa.edu.au>
2024-03-22 20:01:27 +0800

    Update img source and add ajax function

--- js/example.js
+++ js/example.js
@@ -24,3 +24,29 @@ waArea.addEventListener("mouseover", function () {
 waArea.addEventListener("mouseout", function () {
   wa.style.display = "none";
 });
+
+// 当点击超链接时，JavaScript会截取点击事件并执行一个函数
+document.getElementById("ajaxLink").addEventListener("click", function (event) {
+  event.preventDefault(); // 阻止超链接的默认跳转行为
+
+  const ajaxContent = document.getElementById("ajaxContent");
+
+  // 如果内容已经加载，仅切换显示状态
+  if (ajaxContent.innerHTML !== "") {
+    ajaxContent.style.display =
+      ajaxContent.style.display === "none" ? "block" : "none";
+    return; // 提前退出函数
+  }
+
+  // 执行AJAX请求
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      // 使用服务器的响应更新网页内容
+      ajaxContent.style.display = "block";
+      ajaxContent.innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "../ajax_info.txt", true);
+  xhttp.send();
+});
